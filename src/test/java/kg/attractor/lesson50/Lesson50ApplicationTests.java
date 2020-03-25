227e207e87b2174376646ed0ddb85f15140fb80d
dosya_nurbay <dosya@mail.ru>
2020-03-26 01:07:21 +0900

    Initial commit

---
+++ src/test/java/kg/attractor/lesson50/Lesson50ApplicationTests.java
@@ -0,0 +1,13 @@
+package kg.attractor.lesson50;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lesson50ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
